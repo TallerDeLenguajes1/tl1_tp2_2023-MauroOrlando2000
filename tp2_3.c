e955ac99962155c97e5af73dcb65313a68ccbc32
Mauro Orlando <orlandomauro2000@gmail.com>
2023-03-24 12:37:02 -0300

    commit punto 3 completo

--- tp2_3.c
+++ tp2_3.c
@@ -5,19 +5,26 @@
 
 int main()
 {
-    int i, j, *Punt;
-    int mt[N][M];
-    Punt = &mt[0][0];
+    int i, j, n, mt[N][M], *Punt;
+    Punt = mt;
 
     for(i=0; i<N; i++)
     {
         for(j=0; j<M; j++)
         {
-            *(*(Punt + i) + j) = 1 + rand() % 100;
-            printf("%lf ", *Punt);
+            *Punt = 1 + rand() % 100;
+            if(*Punt < 10)
+            {
+                printf("0%d ", *Punt);
+            }
+            else
+            {
+                printf("%d ", *Punt);
+            }
+            Punt++;
         }
         printf("\n");
     }
-
+    
     return 0;
 }
