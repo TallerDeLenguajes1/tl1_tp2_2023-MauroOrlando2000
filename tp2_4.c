ef133c9c1dcbc0f69ec36292aa435cdf9b860276
Mauro Orlando <orlandomauro2000@gmail.com>
2023-03-31 12:49:06 -0300

    commit final punto 4

--- tp2_4.c
+++ tp2_4.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-
-void mostrarDatos(compu PC[5]);
-void PCAntigua(compu PC[5]);
-void PCMasRapida(compu PC[5]);
+#define TAMA 5
 
 struct compu {
     int velocidad;            //entre 1 y 3 GHz
@@ -13,30 +10,35 @@ struct compu {
     char *tipo_cpu;           //valores del arreglo tipos
 } typedef compu;
 
+void mostrarDatos(compu PC[5]);
+void olderPC(compu PC[5]);
+void quickerPC(compu PC[5]);
+
 int main()
 {
     char tipos[6][10]={"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
-    struct compu PC[5], *Punt;
+    struct compu PC[TAMA];
     int i, num;
 
-    for(i=0; i<5; i++)
+    for(i=0; i<TAMA; i++)
     {
-        Punt = &PC[i];
         printf("*****Computadora %d*****\n", i+1);
         printf("¿Que velocidad posee esta computadora? (en GHz): ");
-        scanf("%d", PC[i].velocidad);
-        printf("¿De que anio es esta pc?: ");
-        scanf("%d", PC[i].anio);
+        scanf("%d", &PC[i].velocidad);
+        printf("¿De que anio es esta pc? (entre 2015 y 2023): ");
+        scanf("%d", &PC[i].anio);
         printf("¿Cuantos nucleos posee el procesador de esta pc?: ");
-        scanf("%d", PC[i].cantidad);
+        scanf("%d", &PC[i].cantidad);
         printf("Elija que tipo de cpu lleva integrada la PC\n");
         printf("1=Intel, 2=AMD, 3=Celeron, 4=Athlon, 5=Core, 6=Pentium\n");
         scanf("%d", &num);
-        Punt->tipo_cpu = tipos[(num-1)];
+        PC[i].tipo_cpu = tipos[(num-1)];
         printf("\n\n");
     }
 
     mostrarDatos(PC);
+    olderPC(PC);
+    quickerPC(PC);
 
     return 0;
 }
@@ -45,35 +47,56 @@ void mostrarDatos(compu PC[5])
 {
     int i;
 
-    for(i=0; i<5; i++)
+    for(i=0; i<TAMA; i++)
     {
-        printf("*****Computadora %d*****\n", i+1);
-        printf("Velocidad: %d GHz\n", PC[i].velocidad);
-        printf("Anio de fabricacion: %d\n", PC[i].anio);
-        printf("Cantidad de nucleos: %d\n", PC[i].cantidad);
-        printf("Tipo de cpu: %s\n\n", PC[i].tipo_cpu);
+        printf("**********Computadora %d**********\n", i+1);
+        printf("Procesador %s de %d nucleos de %d GHz\n", PC[i].tipo_cpu, PC[i].cantidad, PC[i].velocidad);
+        printf("Anio de fabricacion: %d\n\n", PC[i].anio);
     }
 }
 
-void PCAntigua(compu PC[5])
+void olderPC(compu PC[5])
 {
-    int i=1, aux=0, anio1, anio2;
-    anio1 = PC[0].anio;
+    int i=0, aux=0, anio;
+    anio = PC[i].anio;
 
-    while(i<5)
+    for(i=1; i<TAMA; i++)
     {
-        anio2 = PC[i].anio;
-        if(anio1 > anio2)
+        if(anio > PC[i].anio)
         {
-            anio1 = anio2;
             aux = i;
+            anio = PC[i].anio;
         }
-        i++;
     }
 
-    printf("*****Computadora %d*****\n", aux);
-    printf("Velocidad: %d GHz\n", PC[aux].velocidad);
-    printf("Anio de fabricacion: %d\n", PC[aux].anio);
-    printf("Cantidad de nucleos: %d\n", PC[aux].cantidad);
-    printf("Tipo de cpu: %s\n\n", PC[aux].tipo_cpu);
+    printf("Computadora mas antigua\n");
+    printf("Procesador %s de %d nucleos de %d GHz\n", PC[aux].tipo_cpu, PC[aux].cantidad, PC[aux].velocidad);
+    printf("Anio de fabricacion: %d\n\n", PC[aux].anio);
+}
+
+void quickerPC(compu PC[5])
+{
+    int i=0, aux=0, speed1, speed2, nucleos;
+    speed1 = PC[i].velocidad;
+    nucleos = PC[i].cantidad;
+
+    for(i=1; i<TAMA; i++)
+    {
+        speed2 = PC[i].velocidad;
+        if(speed1 < speed2)
+        {
+            aux = i;
+        }
+        else if(speed1 == speed2)
+        {
+            if(nucleos < PC[i].cantidad)
+            {
+                aux = i;
+            }
+        }
+    }
+
+    printf("Computadora mas rapida\n");
+    printf("Procesador %s de %d nucleos de %d GHz\n", PC[aux].tipo_cpu, PC[aux].cantidad, PC[aux].velocidad);
+    printf("Anio de fabricacion: %d\n\n", PC[aux].anio);
 }
